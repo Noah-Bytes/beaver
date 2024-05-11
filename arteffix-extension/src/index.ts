65a0b6833066c7084fa13469743eb34b316a7e9c
Crow <yangchen.xiyou@gmail.com>
2024-05-11 22:09:39 +0800

    fix: 到处工具

--- arteffix-extension/src/index.ts
+++ arteffix-extension/src/index.ts
@@ -2,5 +2,6 @@ export * from './extension';
 export * from './image';
 export * from './link';
 export * from './svg';
+export * from './utils';
 export * from './website';
 export * from './websites';
